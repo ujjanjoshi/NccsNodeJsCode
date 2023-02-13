c655ca7e67e9f7c12d83acf45ab53a5b764be6ec
ujjanjoshi <joshiujjan52@gmail.com>
2023-02-13 18:09:08 +0545

    signup updated

--- signup.js
+++ signup.js
@@ -21,39 +21,14 @@ let ab = [{
     "password": "1235"
 }
 ]
-<<<<<<< HEAD
-app.post('/signup', function (req, res) {
-=======
 app.post('/signup', async function (req, res) {
->>>>>>> e4dcc10 (created signuphelper)
     const name = req.body.name;
     const phone = req.body.phone;
     const email = req.body.email;
     const password = req.body.password;
-<<<<<<< HEAD
-    let output = true
-    let pattern = /@gmail.com/;
-    if (name == "" || phone == "" || email == "" || password == "") {
-        output = false
-        res.send("Empty Field")
-    }
-    else if (phone.substring(0, 2) != "98") {
-        output = false
-        res.send("Invalid Phone Number")
-    }
-    else if (password.length != 8) {
-        output = false
-        res.send("Invalid Password(Password should be at least 8 characters)")
-    }
-    else if (pattern.test(email) == false) {
-        output = false
-        res.send("Invalid Email")
-    }
-=======
     let output = await validationHelper.uservalidation(name,phone,email,password)
   
    
->>>>>>> e4dcc10 (created signuphelper)
     if (output == true) {
         console.log("INSERT INTO Signup (name,phone,email,password) VALUES ('"+name+"','"+phone+"','"+email+"','"+password+"')")
         // var sql = "INSERT INTO `login` (username, password) VALUES (" + name + "," + password + ")";
